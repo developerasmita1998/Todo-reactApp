417845f3ad919001026257ef23b1a1ac486f1f0f
Asmita Kumari <asmitababhanlai@gmail.com>
2024-05-22 19:02:29 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Notes = () => {
+  const [listData, setListData] = useState([]); //
+
+  const [inputData, setInputData] = useState("");
+
+  const [editIndex, setEditIndex] = useState(undefined);
+
+  const addData = () => {
+    if (!inputData) {
+      alert("please fill it first");
+      return false;
+    }
+    let temp = [...listData];
+    if (editIndex === undefined) {
+      temp.push(inputData);
+    } else {
+      temp[editIndex] = inputData;
+    }
+
+    setListData(temp);
+    setInputData("");
+    setEditIndex(undefined);
+  };
+
+  const delData = (ind) => {
+    let temp = [...listData];
+    temp.splice(ind, 1);
+    setListData(temp);
+    setInputData("");
+    setEditIndex(undefined);
+  };
+  const editData = (item, ind) => {
+    setInputData(item);
+    setEditIndex(ind);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input
+          type="text"
+          value={inputData}
+          onChange={(e) => setInputData(e.target.value)}
+        />
+        <button onClick={() => addData()}>
+          {editIndex === undefined ? "Add" : "Edit"}
+        </button>
+      </div>
+      <div>
+        {listData.map((item, ind) => (
+          <div>
+            <label>{item}</label>
+            <button onClick={() => delData(ind)}>Del</button>
+            <button onClick={() => editData(item, ind)}>Edit</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
-
-export default App;
+};
+export default Notes;
